d037e21e76dab5e82fe2d52bcacb42e08f39c2c6
Rogerio Prado de Jesus <rogeriopradoj@gmail.com>
2013-09-13 11:25:30 -0300

    Inclui primeira versão Vagrant
    Configs geradas via https://puphpet.com
    
    Feita escolha pela precise32 para aumentar a compatibilidade
    com a ferramenta (certo @williamespindola?)
    
    Signed-off-by: Rogerio Prado de Jesus <rogeriopradoj@gmail.com>

---
+++ vagrant/manifests/default.pp
@@ -0,0 +1,132 @@
+group { 'puppet': ensure => present }
+Exec { path => [ '/bin/', '/sbin/', '/usr/bin/', '/usr/sbin/' ] }
+File { owner => 0, group => 0, mode => 0644 }
+
+class {'apt':
+  always_apt_update => true,
+}
+
+Class['::apt::update'] -> Package <|
+    title != 'python-software-properties'
+and title != 'software-properties-common'
+|>
+
+    apt::key { '4F4EA0AAE5267A6C': }
+
+apt::ppa { 'ppa:ondrej/php5':
+  require => Apt::Key['4F4EA0AAE5267A6C']
+}
+
+class { 'puphpet::dotfiles': }
+
+package { [
+    'build-essential',
+    'vim',
+    'curl',
+    'git-core'
+  ]:
+  ensure  => 'installed',
+}
+
+class { 'apache': }
+
+apache::dotconf { 'custom':
+  content => 'EnableSendfile Off',
+}
+
+apache::module { 'rewrite': }
+
+apache::vhost { 'liga-solidaria-storage.localhost':
+  server_name   => 'liga-solidaria-storage.localhost',
+  serveraliases => [
+],
+  docroot       => '/var/www/',
+  port          => '80',
+  env_variables => [
+    'APP_ENV dev'
+  ],
+  priority      => '1',
+}
+
+class { 'php':
+  service             => 'apache',
+  service_autorestart => false,
+  module_prefix       => '',
+}
+
+php::module { 'php5-mysql': }
+php::module { 'php5-cli': }
+php::module { 'php5-curl': }
+php::module { 'php5-intl': }
+php::module { 'php5-mcrypt': }
+
+class { 'php::devel':
+  require => Class['php'],
+}
+
+class { 'php::pear':
+  require => Class['php'],
+}
+
+
+
+
+
+class { 'xdebug':
+  service => 'apache',
+}
+
+class { 'composer':
+  require => Package['php5', 'curl'],
+}
+
+puphpet::ini { 'xdebug':
+  value   => [
+    'xdebug.default_enable = 1',
+    'xdebug.remote_autostart = 0',
+    'xdebug.remote_connect_back = 1',
+    'xdebug.remote_enable = 1',
+    'xdebug.remote_handler = "dbgp"',
+    'xdebug.remote_port = 9000'
+  ],
+  ini     => '/etc/php5/conf.d/zzz_xdebug.ini',
+  notify  => Service['apache'],
+  require => Class['php'],
+}
+
+puphpet::ini { 'php':
+  value   => [
+    'date.timezone = "America/Sao_Paulo"'
+  ],
+  ini     => '/etc/php5/conf.d/zzz_php.ini',
+  notify  => Service['apache'],
+  require => Class['php'],
+}
+
+puphpet::ini { 'custom':
+  value   => [
+    'display_errors = On',
+    'error_reporting = -1'
+  ],
+  ini     => '/etc/php5/conf.d/zzz_custom.ini',
+  notify  => Service['apache'],
+  require => Class['php'],
+}
+
+
+class { 'mysql::server':
+  config_hash   => { 'root_password' => 'root' }
+}
+
+mysql::db { 'my_database':
+  grant    => [
+    'ALL'
+  ],
+  user     => 'my_user',
+  password => 'my_pass',
+  host     => 'localhost',
+  charset  => 'utf8',
+  require  => Class['mysql::server'],
+}
+
+
