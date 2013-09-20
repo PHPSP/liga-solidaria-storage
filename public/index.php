eb9af2ff23fc2ab521c21cc83cd99b36c9b52cca
Hussani Oliveira <contato@hussani.com.br>
2013-09-20 17:56:35 -0300

    Adicionando rotinas para twig

--- public/index.php
+++ public/index.php
@@ -5,6 +5,7 @@ require __DIR__ . '/../bootstrap.php';
 use Respect\Rest\Router;
 
 $r = new Router;
+$r->isAutoDispatched = false;
 
 $r->any(
     '/**',
@@ -12,3 +13,15 @@ $r->any(
         return 'Olá!';
     }
 );
+
+$r->always(
+    'Accept',
+    array(
+        'text/html'        => new LigaSolidariaStorage\Routine\Twig,
+        'text/plain'       => $json = new LigaSolidariaStorage\Routine\Json,
+        'application/json' => $json,
+        'text/json'        => $json
+    )
+);
+
+print $r->run();
