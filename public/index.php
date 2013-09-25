3bb1ccd0fe05659c62a6a98069f0104a0b2f2d93
leoqbc <tumadjian@gmail.com>
2013-09-25 02:29:05 -0300

    Cria closure de autenticacao, adiciona closure nos routers
    Esta hard coded no index no momento, mas eh so por uma questao inicial o ideial eh implementar em Controllers, fica para uma segunda etapa.

--- public/index.php
+++ public/index.php
@@ -4,17 +4,37 @@ require __DIR__ . '/../bootstrap.php';
 
 use Respect\Rest\Router;
 
+// Esta closure esta aqui somente para reutilizarmos nas
+// chamadas do Router o ideal seria termos uma classe
+// de autenticacao e o codigo separado, como não há tempo
+// ficou assim, por enquanto...
+$isauth = function ($user, $pass) use ($c) {
+    
+    $authenticated = $user === $c->user && $pass === $c->pass;
+    
+    if (!$authenticated) {
+        header('HTTP/1.1 401');
+        header("WWW-Authenticate: Basic realm=\"Login\"");
+    
+        exit('Login cancelado');
+    }
+    return $authenticated;
+};
+
 $r = new Router;
 $r->isAutoDispatched = false;
 
-$r->get('/list/*', 'LigaSolidariaStorage\Storage\Controller\ArtefatoListController' );
+$r->get('/list/*', 'LigaSolidariaStorage\Storage\Controller\ArtefatoListController' )
+        ->authBasic('Acesso', $isauth);
 
-$r->any('/upload', 'LigaSolidariaStorage\Storage\Controller\ArtefatoUploadController');
+$r->any('/upload', 'LigaSolidariaStorage\Storage\Controller\ArtefatoUploadController')
+        ->authBasic('Acesso', $isauth);
+
 
 $r->any(
     '/**',
     function () {
-        return 'Olá!';
+        header('Location: /list');
     }
 );
 
